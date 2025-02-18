353676e9f40169e5f1d69f690d99b4790446b64d
SmiguelRbr10 <miguelrubens2008@gmail.com>
2025-02-18 09:34:54 -0300

    aula 03

---
+++ Aula 03/userservice.js
@@ -0,0 +1,21 @@
+const User = require('./user')
+
+class userService{
+    constuctor(){
+        this.users = []
+        this.nextId = 1
+    }
+
+    addUser(nome, email){
+        const user = new User(this.nextId++, nome, email)
+        this.users.push(user)
+        return user
+    }
+
+    getUsers(){
+        return this.users
+    }
+
+}
+
+module.exports = new userService
