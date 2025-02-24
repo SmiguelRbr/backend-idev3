c2b7f3eaefb50e866095f36fab1a0c1d87c1debb
SmiguelRbr <miguelrubens2008@gmail.com>
2025-02-24 17:07:34 -0300

    a

--- Aula 03/userservice.js
+++ Aula 03/userservice.js
@@ -1,11 +1,34 @@
 const User = require('./user')
 const path = require('path')
 const fs = require ('fs')
+const { error } = require('console')
 
 class userService{
     constuctor(){
+        this.filePath = path.join(__dirname, 'user.json')
         this.users = []
         this.nextId = 1
+    }
+
+    loadUser(){
+        try{
+       
+            if(fs.existsSync(this.filePath)){
+                const data = fs.readFileSync(this.filePath)
+                return JSON.parse(data)
+            }
+        }catch(erro){
+            console.log('Erro ao carregar o arquivo', erro)
+        }
+    }
+
+    getNextID(){
+        try{
+            if(this.users.lenght === 0) return 1;
+            return Math.max(...this.users.map(user => user.id))+1
+        }catch(erro){
+            console.log('Erro ao buscar o proximo id', erro)
+        }
     }
 
     addUser(nome, email){
@@ -18,6 +41,10 @@ class userService{
         return this.users
     }
 
+  
+
 }
+
+
 
 module.exports = new userService
